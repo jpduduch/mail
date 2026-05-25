03945e50ab902d1b21b6a06a7e0318a894294a48
João Duduch <jpduduch@gmail.com>
2026-05-25 16:07:23 +0200

    feat: view email

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -17,14 +17,16 @@ function compose_email() {
 
     // Show compose view and hide other views
     document.querySelector('#emails-view').style.display = 'none';
+    document.querySelector('#email-view').style.display = 'none';
     document.querySelector('#compose-view').style.display = 'block';
-    clear();
+    clear_composition_fields();
 }
 
 function load_mailbox(mailbox) {
 
     // Show the mailbox and hide other views
     document.querySelector('#emails-view').style.display = 'block';
+    document.querySelector('#email-view').style.display = 'none';
     document.querySelector('#compose-view').style.display = 'none';
 
     // Show the mailbox name
@@ -61,7 +63,7 @@ function send_mail(event) {
         if ("error" in result) {
             feedback_message('#compose-view > h3', result.error);
         } else {
-            clear();
+            clear_composition_fields();
             load_mailbox('sent');
             feedback_message('#emails-view > h3', result.message);
         }
@@ -70,18 +72,23 @@ function send_mail(event) {
 
 function render_emails(emails) {
 
-    const email_list = document.querySelector('#emails-view');
+    const email_list_group = document.querySelector('#emails-view');
+
+    if (Object.keys(emails).length === 0) {
+        email_list_group.innerHTML = `<p>No emails in this mailbox.</p>`
+        return
+    }
 
     emails.forEach(email => {
 
-        const a = document.createElement('a');
-        a.classList.add('list-group-item', 'list-group-item-action');
-        a.setAttribute('href', '#');
-        email_list.append(a);
+        const email_list_item = document.createElement('a');
+        email_list_item.classList.add('list-group-item', 'list-group-item-action');
+        email_list_item.setAttribute('href', `/emails/${email.id}`);
+        email_list_group.append(email_list_item);
 
         const div = document.createElement('div');
         div.classList.add('d-flex', 'w-100', 'justify-content-between');
-        a.append(div);
+        email_list_item.append(div);
 
         const title = document.createElement('h5');
         title.classList.add('mb-1');
@@ -95,19 +102,69 @@ function render_emails(emails) {
 
         const sender = document.createElement('p');
         sender.classList.add('mb-1');
-        a.append(sender);
+        email_list_item.append(sender);
         sender.innerHTML = email.sender;
 
         if (email.read) {
-            a.classList.add('list-group-item-secondary');
+            email_list_item.classList.add('list-group-item-secondary');
         }
+
+        email_list_item.addEventListener('click', (event) => {
+            event.preventDefault();
+
+            fetch(`emails/${email.id}`)
+            .then(response => response.json())
+            .then(email => {
+                render_email(email);
+            })
+        });
     });
+}
+
+function render_email(email) {
+    console.log(email);
+
+    // Clear inbox to clear memory usage
+    document.querySelector('#emails-view').innerHTML = ''
+
+    // Show email view and hide other views
+    document.querySelector('#emails-view').style.display = 'none';
+    document.querySelector('#email-view').style.display = 'block';
+    document.querySelector('#compose-view').style.display = 'none';
+
+    // Clear any previous content
+    const email_body = document.querySelector('#email-view');
+    email_body.innerHTML = '';
+
+    email_body.innerHTML = `
+        <ul class="list-unstyled">
+            <li><strong>From:</strong> ${email.sender}</li>
+            <li><strong>To:</strong> ${email.recipients}</li>
+            <li><strong>Subject:</strong> ${email.subject}</li>
+            <li><strong>Date:</strong> ${email.timestamp}</li>
+        </ul>
+
+        <hr>
+    `
+
+    const body = document.createElement('section');
+    email_body.append(body);
+
+    body.innerHTML = email.body;
+
+    // Mark as read
+    fetch(`/emails/${email.id}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+            read: true
+        })
+    })
 }
 
 
 // Utils
 
-function clear() {
+function clear_composition_fields() {
     // Clear out composition fields
     document.querySelector('#compose-recipients').value = '';
     document.querySelector('#compose-subject').value = '';
