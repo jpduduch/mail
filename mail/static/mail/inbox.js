45cd4c03ae179b8c027ee0408a0a0321f891834f
João Duduch <jpduduch@gmail.com>
2026-06-05 15:30:08 +0200

    chore: elevate mailbox component to mainview

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -12,32 +12,24 @@ function App() {
 
     function handleActiveView(name) {
         setActiveView(name.toLowerCase())
-    }
 
-    function renderView() {
-        switch (activeView) {
-            case 'compose':
-                console.log(activeView);
-                break
-            case 'email':
-                console.log(activeView);
-                break
-            default:
-                return <MailBox category={activeView} />
-        }
     }
 
     return(
-        <nav>
-            {
-                views.map(view => {
-                    return <TabItem key={view} name={view} onSelect={() => handleActiveView(view)}  />
-                })
-            }
+        <div>
+            <nav>
+                {
+                    views.map(view => {
+                        return <TabItem key={view} name={view} onSelect={() => handleActiveView(view)}  />
+                    })
+                }
+            </nav>
 
-            {renderView()}
+            <hr />
 
-        </nav>
+            <MainView category={activeView} onSelect={handleActiveView} />
+
+        </div>
     )
 }
 
@@ -51,21 +43,85 @@ function TabItem({name, onSelect}) {
     )
 }
 
-function MailBox({category, onSelect}) {
+function MainView({category, onSelect}) {
 
     const [emails, setEmails] = React.useState([]);
 
+    const isMailbox = (category === 'inbox' || category === 'sent' || category === 'archived') ? true : false;
+
     React.useEffect(() => {
+
+        if (!isMailbox) {
+            return;
+        }
+
         fetch(`/emails/${category}`)
         .then(response => response.json())
-        .then(data => {
-            setEmails(data)
+        .then(metadata => {
+            setEmails(metadata);
         })
     }, [category])
 
+    if (!isMailbox) {
+        return (
+            <div>
+                <ComposeForm />
+            </div>
+        );
+    }
+
     return (
         <div>
-            {emails.map(data => console.log(data))}
+            <h2>{category.charAt(0).toUpperCase() + category.slice(1)}</h2>
+            {emails.map(metadata => <MailListItem metadata={metadata} />)}
+        </div>
+    )
+}
+
+function MailListItem({metadata, onSelect}) {
+    const isRead = metadata.read ? 'list-group-item-secondary' : '';
+
+    function loadMail(event, id) {
+        event.preventDefault();
+    }
+
+    return (
+        <div>
+            <a
+                href={`emails/${metadata.id}`}
+                className={`list-group-item list-group-item-action mb-1 ${isRead}`}
+                onClick={(event) => { loadMail(event, metadata.id) }}
+            >
+                <div className="d-flex w-100 justify-content-between">
+                    <h5 className="mb-1">{metadata.subject}</h5>
+                    <small className="text-body-secondary">{metadata.timestamp}</small>
+                </div>
+                <p className="mb-1">{metadata.sender}</p>
+            </a>
+        </div>
+    )
+}
+
+function ComposeForm() {
+    return (
+        <div>
+            <h2>
+                New Mail
+            </h2>
+
+            <form>
+                <div className="form-group">
+                    From: <input disabled className="form-control" value="Sender (you)" />
+                </div>
+                <div className="form-group">
+                    To: <input className="form-control" />
+                </div>
+                <div className="form-group">
+                    <input className="form-control" placeholder="Subject" />
+                </div>
+                <textarea class="form-control mb-1" id="compose-body" placeholder="Body"></textarea>
+                <input type="submit" class="btn btn-primary" />
+            </form>
         </div>
     )
 }
